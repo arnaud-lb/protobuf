91937bb2803f1c3d99ee969ed2e5a6c0718601c3
Adam Cozzette <acozzette@google.com>
2018-06-13 14:22:04 -0700

    Added Kokoro Windows release build for protoc

---
+++ kokoro/windows/protoc/build.bat
@@ -0,0 +1,18 @@
+set generator=Visual Studio 14 Win64
+set vcplatform=x64
+set configuration=Release
+set BUILD_DLL=ON
+set UNICODE=ON
+
+:: Build and run C++ tests
+echo Building C++
+mkdir build_msvc
+cd build_msvc
+cmake -G "%generator%" -Dprotobuf_BUILD_SHARED_LIBS=%BUILD_DLL% -Dprotobuf_UNICODE=%UNICODE% ../cmake
+msbuild protobuf.sln /p:Platform=%vcplatform% || goto error
+cd %configuration%
+goto :EOF
+
+:error
+echo Failed!
+exit /b %ERRORLEVEL%
